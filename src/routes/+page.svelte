74d05e5141d956c8dd522b74db541a64334dae90
Arnis Vuskans <ampatspell@gmail.com>
2025-10-03 22:24:20 +0300

    one more time

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="hello">Hello, I'm SvelteKit app on Dokku named demo-frontend.</div>
+
+<style lang="scss">
+  .hello {
+    padding: 50px;
+  }
+</style>
